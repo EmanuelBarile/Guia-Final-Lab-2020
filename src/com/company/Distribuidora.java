e4a43d70018df0b43a22fa8c0f22d78abaa463f7
unknown <emanuelbarile06@gmail.com>
2021-05-05 19:00:41 -0300

    version final optimizada

--- src/com/company/Distribuidora.java
+++ src/com/company/Distribuidora.java
@@ -83,6 +83,76 @@ public class Distribuidora {
         }
     }
 
+    public  void imprimirProductos() {
+        System.out.println("\n///// Listado de  productos /////"); // probar declararlo en clase distribuidora
+        for (Producto produ : this.getListaProductos() ) {
+            if (produ != null) System.out.println(produ);
+        }
+    }
+
+    public  void  imprimirClientes ( ){
+        System.out.println("\n////// Listado de clientes ///// " );
+        for (Cliente cliente  : this.getListaClientes()  ) {
+            if(cliente !=null) System.out.println(cliente);
+
+        }
+
+    }
+
+    public  void imprimirEnvios (){
+
+        System.out.println("\n////// Listado de envios ///// ");
+        for ( Envio envio : this.getListaEnvios() ) {
+            if (envio !=null) System.out.println(envio);
+
+        }
+    }
+
+    //4) Realizar 5 ventas por consola y mostrar el promedio de totales vendidos (costo
+    //del producto + envío).
+
+    public  double promedioDeTotalVendido(){
+
+        double promedioTotal=0;
+        for (Envio envio : this.getListaEnvios() ) {
+            if(envio !=null){
+                promedioTotal+= Envio.calcularCostoTotal(envio.getClienteEnvio(), envio.getProductoEnvio(), envio.getCantidadKm());
+            }
+        }
+        return promedioTotal / this.getListaEnvios().size();
+    }
+
+    //5) Indicar por consola, quien fue el cliente que más compras realizó y mostrar sus
+    //atributos
+
+    public  void imprimirClienteConMasCompras(){
+
+        int contador=0;     // guardo las frecuencias al terminar cada ronda
+        int frecuencia=0;   // cuento las frecuencias de cada cliente
+        int indice=-1;      // -1 para marcar el primer sub indice en 0
+        int j=0;
+
+        for (int i=0; i<this.getListaClientes().size(); i++){
+            Cliente cliente= this.getListaClientes().get(i); // guardo el cliente de primer sub indice
+
+            for (j=0; j<this.getListaEnvios().size();j++){
+                if (cliente.equals(this.getListaEnvios().get(j).getClienteEnvio()) ){ // lo comparo con el cliente de la lista de envios
+                    frecuencia++; // acumulo frecuencia de coincidencias
+                }
+
+            }
+            j=0; // reseteo j para recorrer la listaEnvios desde el principio en la proxima vuelta
+            if (frecuencia>contador){   //si la frecuencia de la reciente vuelta es mayor a contador hago los reemplazos
+                indice++;               // guardo el indice del cliente con mayor cantidad de compras
+                contador=frecuencia;    // guardo las frecuencias en contador
+                frecuencia=0;           // reseteo frecuencias
+            }
+        }
+
+        System.out.println("Cliente con mayor cantidad de compras es: "+ this.getListaClientes().get(indice));
+        System.out.println("con un total de ventas igual a :"+contador );
+    }
+
 
     @Override
     public String toString() {
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -24,8 +24,8 @@ public class Main {
         distribuidoraUnica.insertarProducto("Cubetera",36,60);
 
 
-        imprimirProductos(distribuidoraUnica);
-        imprimirClientes(distribuidoraUnica);
+       distribuidoraUnica.imprimirProductos();
+       distribuidoraUnica.imprimirClientes();
 
         //cargo envios
 
@@ -37,85 +37,19 @@ public class Main {
         distribuidoraUnica.realizarUnEnvio("Jose","Destapador",28);
         distribuidoraUnica.realizarUnEnvio("Jose","Llavero",77);
 
-        imprimirEnvios(distribuidoraUnica);
-        imprimirProductos(distribuidoraUnica);
+        distribuidoraUnica.imprimirEnvios();
+        distribuidoraUnica.imprimirProductos();
 
-        System.out.println("el promedio total de vendidos es: "+ promedioDeTotalVendido(distribuidoraUnica));
-        imprimirClienteConMasCompras(distribuidoraUnica);
-
-    }
-
-
-    // Métodos estáticos , duda de si declararlos acá en main o en la clase Distribuidora
-
-    public static void imprimirProductos(Distribuidora distribuidora ) {
-        System.out.println("\n///// Listado de  productos /////"); // probar declararlo en clase distribuidora
-        for (Producto produ : distribuidora.getListaProductos() ) {
-            if (produ != null) System.out.println(produ);
-        }
-    }
-
-    public static void  imprimirClientes (Distribuidora distribuidora){
-        System.out.println("\n////// Listado de clientes ///// " );
-        for (Cliente cliente  : distribuidora.getListaClientes()  ) {
-            if(cliente !=null) System.out.println(cliente);
-
-        }
+        System.out.println("el promedio total de vendidos es: "+ distribuidoraUnica.promedioDeTotalVendido());
+        distribuidoraUnica.imprimirClienteConMasCompras();
 
     }
 
-    public static void imprimirEnvios (Distribuidora distribuidora){
 
-        System.out.println("\n////// Listado de envios ///// ");
-        for ( Envio envio : distribuidora.getListaEnvios() ) {
-            if (envio !=null) System.out.println(envio);
 
-        }
-    }
 
-    //4) Realizar 5 ventas por consola y mostrar el promedio de totales vendidos (costo
-    //del producto + envío).
 
-    public static double promedioDeTotalVendido(Distribuidora distribuidora){
 
-        double promedioTotal=0;
-        for (Envio envio : distribuidora.getListaEnvios() ) {
-            if(envio !=null){
-                promedioTotal+= Envio.calcularCostoTotal(envio.getClienteEnvio(), envio.getProductoEnvio(), envio.getCantidadKm());
-            }
-        }
-        return promedioTotal / distribuidora.getListaEnvios().size();
-    }
 
-    //5) Indicar por consola, quien fue el cliente que más compras realizó y mostrar sus
-    //atributos
-
-    public static void imprimirClienteConMasCompras(Distribuidora distribuidora){
-
-        int contador=0;     // guardo las frecuencias al terminar cada ronda
-        int frecuencia=0;   // cuento las frecuencias de cada cliente
-        int indice=-1;      // -1 para marcar el primer sub indice en 0
-        int j=0;
-
-        for (int i=0; i<distribuidora.getListaClientes().size(); i++){
-            Cliente cliente= distribuidora.getListaClientes().get(i); // guardo el cliente de primer sub indice
-
-            for (j=0; j<distribuidora.getListaEnvios().size();j++){
-                if (cliente.equals(distribuidora.getListaEnvios().get(j).getClienteEnvio()) ){ // lo comparo con el cliente de la lista de envios
-                    frecuencia++; // acumulo frecuencia de coincidencias
-                }
-
-            }
-            j=0; // reseteo j para recorrer la listaEnvios desde el principio en la proxima vuelta
-            if (frecuencia>contador){   //si la frecuencia de la reciente vuelta es mayor a contador hago los reemplazos
-                indice++;               // guardo el indice del cliente con mayor cantidad de compras
-                contador=frecuencia;    // guardo las frecuencias en contador
-                frecuencia=0;           // reseteo frecuencias
-            }
-        }
-
-        System.out.println("Cliente con mayor cantidad de compras es: "+ distribuidora.getListaClientes().get(indice));
-        System.out.println("con un total de ventas igual a :"+contador );
-    }
 
 }
